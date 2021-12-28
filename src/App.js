15c419664cac2614104630c9ffd505e218e6f49c
Steve Correa <s.correa@berkeley.edu>
2021-12-28 13:00:25 -0800

    on phase 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
+import GalleryNavigation from './components/GalleryNavigation';
+import harvardArt from "./data/harvardArt"
 
 function App() {
+
   return (
     <div>
       <h1>React App</h1>
